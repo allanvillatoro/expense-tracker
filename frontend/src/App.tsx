e0ce73cc04eb3967736a48f72439ad7397002258
Allan Villatoro <allanvillatoro@unitec.edu>
2022-07-27 17:18:33 -0600

    adding frontend template

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Styles.css'
+
+function App() {
+  return (
+    <div>
+      <header>
+      </header>
+      Expense Tracker
+    </div>
+  );
+}
+
+export default App;
