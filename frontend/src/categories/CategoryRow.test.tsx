0fe17e5963af77f6d8f55dfc500b5fad834c4325
Allan Villatoro <allanvillatoro@unitec.edu>
2022-07-28 14:24:54 -0600

    CategoryRow test added

---
+++ frontend/src/categories/CategoryRow.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import { CategoryRow } from './CategoryRow';
+import { categoryStub } from './categoryStub';
+
+test("renders CategoryRow component with 3 cells", () => {
+  render(
+    <table>
+      <tbody>
+        <CategoryRow category={categoryStub} />
+      </tbody>
+    </table>
+  );
+  //screen.debug();
+  //it contains 3 cells
+  expect(screen.getAllByRole("cell").length).toBe(3);
+  //it contains the word pizza
+  expect(screen.getByText("food")).toBeTruthy();
+});
---
+++ frontend/src/categories/CategoryRow.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CategoryRow = () => {
+  return (
+    <div>CategoryRow</div>
+  )
+}
