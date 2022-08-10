d872a6e7db4dd788fddaa54a2b77d477b0ffe380
Allan Villatoro <allanvillatoro@unitec.edu>
2022-08-09 22:57:40 -0600

    adding MonthlyReportPage test

---
+++ frontend/src/home/MonthlyReportPage.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../app/store";
+import { MonthlyReportPage } from "./MonthlyReportPage";
+
+jest.mock("react-chartjs-2", () => ({
+  Bar: () => null,
+}));
+
+test("should render MonthlyReportPage", () => {
+  render(
+    <Provider store={store}>
+      <MonthlyReportPage />
+    </Provider>
+  );
+  screen.debug();
+
+  expect(screen.getByRole("combobox")).toHaveDisplayValue("Select a month");
+  expect(screen.getAllByRole("option").length).toBe(2);
+});
