e89d62444ac0bb46f452967b0a18e3cd3b97a59e
Allan Villatoro <allanvillatoro@unitec.edu>
2022-08-09 21:30:50 -0600

    adding tests to App component

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,21 +1,74 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { act, render, screen } from "@testing-library/react";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { UserLoginForm } from "./interfaces/UserPost";
+import { login } from "./users/usersSlice";
+import { User } from "./interfaces/User";
+import { usersApi } from "./api/usersApi";
+import userEvent from "@testing-library/user-event";
 
-test('should render App component', () => {
+const fakeLoggedUser: User = {
+  _id: "fakeId",
+  email: "tom@react.com",
+  fullName: "Tom Johnson",
+};
+
+test("should render App component", async () => {
+  //renders the app component
   render(
     <Provider store={store}>
-      <App/>
+      <App />
     </Provider>
   );
-  //screen.debug();
+
+  //mocks the login function on usersApi
+  const mock = jest.spyOn(usersApi, "login").mockResolvedValue({
+    data: { ...fakeLoggedUser },
+    status: 201,
+    statusText: "OK",
+    headers: {},
+    config: {},
+  });
+
+  //fake login user
+  const newLogin: UserLoginForm = {
+    email: "tom@react.com",
+    password: "reactRedux2022",
+  };
+
+  //login user
+  await act(async () => {
+    //calls the mock function
+    const result = await store.dispatch(login(newLogin));
+    const loggedUser: User = result.payload as User;
+    expect(result.type).toBe("users/login/fulfilled");
+    //should be logged in
+    const state = store.getState().users;
+    expect(state.isLoggedIn).toBeTruthy();
+  });
 
   //it will work only if it's logged in
-  //expect(screen.getByRole("navigation")).toBeInTheDocument()
-  //expect(screen.getAllByRole("link").length).toBe(4);
-  //expect(screen.findByRole("link", { name: "Home"})).toBeInTheDocument();
-  //expect(screen.findByRole("link", { name: "Expenses"})).toBeInTheDocument();
-  //expect(screen.findByRole("link", { name: "Categories"})).toBeInTheDocument();
-  //expect(screen.findByRole("link", { name: "About"})).toBeInTheDocument();
+  //displays the nav bar
+  await act(async () => {
+    expect(screen.getByRole("navigation")).toBeInTheDocument();
+
+    expect(screen.getByRole("link", { name: "Home" })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "Expenses" })).toBeInTheDocument();
+    expect(
+      screen.getByRole("link", { name: "Categories" })
+    ).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "About" })).toBeInTheDocument();
+
+    //clicks on About link to render About page
+    userEvent.click(screen.getByText(/About/i));
+  });
+
+  //it will work only if it's logged in
+  //it should render 5 links (home, expenses, categories, about and the ExpenseTracker brand)
+  //App is now rendering the About component 
+  await act(async () => {
+    //screen.debug()
+    expect(screen.getAllByRole("link").length).toBe(5);
+  });
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -29,9 +29,9 @@ function App() {
           {isLoggedIn && (
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
               <div className="container-fluid">
-                <a className="navbar-brand" href="#">
+                <Link className="navbar-brand" to="/">
                   ExpenseTracker
-                </a>
+                </Link>
                 <button
                   className="navbar-toggler"
                   type="button"
